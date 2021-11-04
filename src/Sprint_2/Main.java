5705fea31a0791b311c6a06646c10ffba27c165d
Андрей Степанов <armlet71@mail.ru>
2021-11-04 12:29:40 +0300

    test

--- src/Sprint_2/Main.java
+++ src/Sprint_2/Main.java
@@ -5,5 +5,19 @@ public class Main {
 
 
     }
+
+    public static class Task_6_1 {
+        public static void main(String[] args) {
+
+            System.out.println(sumOfTwoNumbers(3, 2));
+            System.out.println(sumOfTwoNumbers(-3, -6));
+            System.out.println(sumOfTwoNumbers(7, 3));
+
+        }
+
+        static int sumOfTwoNumbers(int x, int y) {
+            return x + y;
+        }
+    }
 }
 
